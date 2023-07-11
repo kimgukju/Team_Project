45fd079486ce5bab1ba87d0816d524ac0c825472
kimgukju <kimgukju@kimgukju.com>
2023-07-11 16:44:56 +0900

    0711

---
+++ BusTable/src/main/java/com/project/BusTable/BusTableApplication.java
@@ -0,0 +1,16 @@
+package com.project.BusTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class BusTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTableApplication.class, args);
+	}
+
+
+}
---
+++ BusTable/src/test/java/com/project/BusTable/BusTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BusTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
